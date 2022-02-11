489d5365bb458308707609e2a0905604c01a5fd4
Yash Gupta <yashguptaab99@gmail.com>
2022-02-11 22:43:14 +0530

    Mine Block API

--- src/controllers/chain.js
+++ src/controllers/chain.js
@@ -1,5 +1,6 @@
 import { successResponse } from '../core/apiResponse.js';
 import { httpStatusCode } from '../core/index.js';
+import Block from '../utils/block.js';
 
 export function getChain(req, res) {
   const chain = req.app.locals.blockchain;
@@ -16,6 +17,20 @@ export function validateChain(req, res) {
     chain.isChainValid()
       ? 'The Blockchain is valid.'
       : 'The Blockchain is invalid.',
+    httpStatusCode.OK,
+  );
+}
+
+export function mineBlock(req, res) {
+  const chain = req.app.locals.blockchain;
+  chain.addBlock(
+    new Block(chain.getLatestBlock().index + 1, Date.now(), req.body),
+  );
+  return successResponse(
+    req,
+    res,
+    chain.getLatestBlock(),
+    'A block is MINED',
     httpStatusCode.OK,
   );
 }
--- src/routes/api/v1/chain.js
+++ src/routes/api/v1/chain.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/get-chain', chainController.getChain);
 router.get('/validate-chain', chainController.validateChain);
+router.post('/mine-block', chainController.mineBlock);
 
 export default router;
