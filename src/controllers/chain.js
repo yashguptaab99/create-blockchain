f6eff1867651b65c9d35b854d5172509052a759d
Yash Gupta <yashguptaab99@gmail.com>
2022-02-11 22:28:24 +0530

    Validate Chain API

--- src/controllers/chain.js
+++ src/controllers/chain.js
@@ -1,14 +1,21 @@
 import { successResponse } from '../core/apiResponse.js';
 import { httpStatusCode } from '../core/index.js';
 
-// eslint-disable-next-line import/prefer-default-export
 export function getChain(req, res) {
   const chain = req.app.locals.blockchain;
+  return successResponse(req, res, chain, 'All Chain Data', httpStatusCode.OK);
+}
+
+export function validateChain(req, res) {
+  const chain = req.app.locals.blockchain;
+
   return successResponse(
     req,
     res,
-    chain,
-    'Hello',
+    chain.isChainValid(),
+    chain.isChainValid()
+      ? 'The Blockchain is valid.'
+      : 'The Blockchain is invalid.',
     httpStatusCode.OK,
   );
 }
--- src/routes/api/v1/chain.js
+++ src/routes/api/v1/chain.js
@@ -4,4 +4,6 @@ import { chainController } from '../../../controllers/index.js';
 const router = express.Router();
 
 router.get('/get-chain', chainController.getChain);
+router.get('/validate-chain', chainController.validateChain);
+
 export default router;
